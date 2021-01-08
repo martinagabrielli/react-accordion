83edf2f2bf0e2d456ae976d8aa0042004cd7f904
martinagabrielli <martina.gabrielli@yahoo.it>
2021-01-08 17:45:03 +0000

    added latest changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import data from './data'
+import SingleQuestion from './Question'
 
 function App() {
+  const [questions, setQuestions] = useState(data)
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
-  );
+    <main>
+      <div className="container">
+        <h3>Questions and answers about login</h3>
+        <section className="info">
+          {
+            questions.map((question) => {
+              return <SingleQuestion key={question.id} {...question}/>
+            })
+          }
+        </section>
+      </div>
+    </main>
+  )
 }
 
-export default App;
+export default App
